bbc82e349d3c000b45f70c409f551b864dccc915
Joaquín Fernández Campo <jfcampo@gmail.com>
2020-12-09 12:28:55 +0100

    added optimized version for second part based on https://github.com/T-Dark0/Advent-Of-Code-2020-day9/blob/master/src/main.rs
    Signed-off-by: Joaquin Fernandez Campo <jfcampo@gmail.com>

--- 2020/09/second_optimized.go
+++ 2020/09/second_optimized.go
@@ -17,7 +17,7 @@ func main() {
 		converted, _ := strconv.Atoi(s)
 		numbers = append(numbers, converted)
 	}
-	targetNumber := make([]int, 0, 100)
+	targetNumber := 0
 	for i := preambleLength; i < len(numbers); i++ {
 		preamble := make(map[int]bool, preambleLength)
 		found := false
@@ -34,40 +34,39 @@ func main() {
 		}
 
 		if !found {
-			targetNumber = append(targetNumber, candidate)
-			fmt.Printf("Bad number: %d\n", candidate)
+			targetNumber = candidate
+			break
 		}
 	}
 
-	fmt.Printf("Found %d candidates: %+v\n", len(targetNumber), targetNumber)
+	fmt.Printf("Found candidates: %+v\n", targetNumber)
 	startRange := 0
-	endRange := 1
-
-	firstBadNumber := targetNumber[0]
+	endRange := 0
 	totalSums := 0
+	var currentSum int
 	for {
-		currentSum := 0
-		minNumber := firstBadNumber + 1
-		maxNumber := 0
-		for i := startRange; i < endRange; i++ {
-			totalSums++
-			currentSum += numbers[i]
-			if numbers[i] > maxNumber {
-				maxNumber = numbers[i]
-			}
-			if numbers[i] < minNumber {
-				minNumber = numbers[i]
-			}
-		}
-		if currentSum == firstBadNumber {
+		totalSums++
+		if currentSum == targetNumber {
 			fmt.Printf("Found range! %d, %d after %d sums\n", startRange, endRange, totalSums)
-			fmt.Printf("Min in range :%d, max in range:%d, sum :%d\n", minNumber, maxNumber, minNumber+maxNumber)
 			break
-		}
-		if currentSum < firstBadNumber {
+		} else if currentSum < targetNumber {
+			currentSum += numbers[endRange]
 			endRange++
-		} else if currentSum > firstBadNumber {
+		} else if currentSum > targetNumber {
+			currentSum -= numbers[startRange]
 			startRange++
 		}
 	}
+	maxNumber := 0
+	minNumber := currentSum + 1
+	for i := startRange; i < endRange; i++ {
+		if numbers[i] > maxNumber {
+			maxNumber = numbers[i]
+		}
+		if numbers[i] < minNumber {
+			minNumber = numbers[i]
+		}
+	}
+	fmt.Printf("Min in range(%d-%d) :%d, max in range:%d, sum :%d\n", startRange, endRange, minNumber, maxNumber, minNumber+maxNumber)
+
 }
