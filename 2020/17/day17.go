69d955c37752ffadd717dd01caf6ed01adebb301
Joaquín Fernández Campo <jfcampo@gmail.com>
2020-12-17 20:41:36 +0100

    simplified logic and improved fourth dimension handling
    Signed-off-by: Joaquin Fernandez Campo <jfcampo@gmail.com>

--- 2020/17/day17.go
+++ 2020/17/day17.go
@@ -25,8 +25,6 @@ func main() {
 	input, _ := ioutil.ReadFile(*inputFile)
 	trimmedInput := strings.Split(strings.TrimSpace(string(input)), "\n")
 	points := parse(trimmedInput)
-	fmt.Printf("Active before: %d\n", len(points))
-	fmt.Printf("********\n")
 	t := time.Now()
 	for i := 0; i < *iterations; i++ {
 		points = evolve(points, false)
@@ -49,50 +47,6 @@ func main() {
 
 }
 
-func PrintPoints(allPoints map[Point]bool) {
-	minX := 9999999999
-	maxX := 0
-	minY := 9999999999
-	maxY := 0
-	minZ := 9999999999
-	maxZ := 0
-	for k := range allPoints {
-		if k.X < minX {
-			minX = k.X
-		} else if k.X > maxX {
-			maxX = k.X
-		}
-		if k.Y < minY {
-			minY = k.Y
-		} else if k.Y > maxY {
-			maxY = k.Y
-		}
-		if k.Z < minZ {
-			minZ = k.Z
-		} else if k.Z > maxZ {
-			maxZ = k.Z
-		}
-	}
-	var sb strings.Builder
-	for k := minZ; k <= maxZ; k++ {
-		sb.WriteString(fmt.Sprintf("\n**z = %d **\n", k))
-		for i := minX; i <= maxX; i++ {
-			for j := minY; j <= maxY; j++ {
-				p, _ := allPoints[Point{X: i, Y: j, Z: k}]
-				if p {
-					sb.WriteString("#")
-				} else {
-					sb.WriteString(".")
-				}
-			}
-			sb.WriteString("\n")
-		}
-		sb.WriteString(fmt.Sprintf("\n**z = %d **\n", k))
-	}
-	fmt.Printf(sb.String())
-	//fmt.Printf("min x: %d, min y: %d, min z: %d\n", minX, minY, minZ)
-	//fmt.Printf("max x: %d, max y: %d, max z: %d\n", maxX, maxY, maxZ)
-}
 func evolve(allPoints map[Point]bool, enableFourthDimension bool) map[Point]bool {
 
 	result := make(map[Point]bool)
@@ -106,33 +60,34 @@ func evolve(allPoints map[Point]bool, enableFourthDimension bool) map[Point]bool
 		if len(allNeighbors)-totalInactiveNeighbors != 2 && len(allNeighbors)-totalInactiveNeighbors != 3 {
 			continue
 		}
-		_, ok := allPoints[p]
 		numberOfActiveNeighbors := len(allNeighbors) - totalInactiveNeighbors
-		if numberOfActiveNeighbors == 3 && ok == false {
-			//Flip from inactive to active
+		if allPoints[p] == true {
 			result[p] = active
-		} else if ok == true && (numberOfActiveNeighbors == 2 || numberOfActiveNeighbors == 3) {
-			//do nothing
+			//keep active
+		} else if numberOfActiveNeighbors == 3 {
+			//Flip from inactive to active
 			result[p] = active
 		}
 	}
-
 	return result
 }
 func neighbors(allPoints map[Point]bool, p Point, enableFourthDimension bool) ([]Point, int) {
 	var result []Point
 	var inactivePoints = 0
-	for w := -1; w < 2; w++ {
-		if !enableFourthDimension {
-			w = 0
-		}
+	wMin := -1
+	wMax := 2
+	if !enableFourthDimension {
+		wMin = 0
+		wMax = 1
+	}
+	for w := wMin; w < wMax; w++ {
 		for k := -1; k < 2; k++ {
 			for i := -1; i < 2; i++ {
 				for j := -1; j < 2; j++ {
-					checks++
 					if i == 0 && j == 0 && k == 0 && w == 0 {
 						continue
 					}
+					checks++
 					candidatePoint := Point{
 						X: p.X + i,
 						Y: p.Y + j,
@@ -140,14 +95,11 @@ func neighbors(allPoints map[Point]bool, p Point, enableFourthDimension bool) ([
 						W: p.W + w,
 					}
 					result = append(result, candidatePoint)
-					if _, ok := allPoints[candidatePoint]; !ok {
+					if !allPoints[candidatePoint] {
 						inactivePoints++
 					}
 				}
 			}
-		}
-		if !enableFourthDimension {
-			break
 		}
 	}
 	return result, inactivePoints
