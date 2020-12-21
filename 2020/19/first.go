1021228b63f254b3370c300698a60c131c97e1d7
Joaquín Fernández Campo <jfcampo@gmail.com>
2020-12-21 13:15:25 +0100

    part2 done. still don't understand why the recursive solution fails
    Signed-off-by: Joaquin Fernandez Campo <jfcampo@gmail.com>

--- 2020/19/first.go
+++ 2020/19/first.go
@@ -5,96 +5,71 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
 var inputFile = flag.String("f", "input", "Relative file path to use as input.")
+var ruleToCheck = flag.String("r", "0", "Use this rule to check")
 
 //Rule A rule.
 type Rule struct {
-	ruleID             int
+	ruleID             string
 	LRule              []*Rule
 	RRule              []*Rule
-	Expression         string
 	OriginalExpression string
-	chars              string
+	value              string
 	parsed             bool
-	parsing            bool
 }
 
-//MatchText recursively iterates through a string and validates a rule
-var numberOfChecks int
-var visitedRules = make(map[string]bool)
-
 //MatchBranch recursively iterates through a string and validates a rule
-func (r Rule) MatchBranch(currentDepth int, rList []*Rule, input string, lazyRules map[int]bool) (bool, string) {
-	currentDepth++
+func (r Rule) MatchBranch(rList []*Rule, input string) (bool, string) {
 	matches := false
-	localBranchInput := input
+	pendingMatchText := input
+	matchingChars := 0
 	for _, localRule := range rList {
 		//fmt.Printf("Applying rule(%d) - '%s' for input '%s'\n", localRule.ruleID, localRule.String(), inputToMatch)
-		matches, localBranchInput = localRule.MatchText(currentDepth, localBranchInput, lazyRules)
+		i := pendingMatchText
+		matches, pendingMatchText = localRule.MatchText(input[matchingChars:])
 		if !matches {
-			break
+			return matches, ""
 		}
+		matchingChars += len(i) - len(pendingMatchText)
 	}
-	return matches, localBranchInput
+	return matches, pendingMatchText
 }
 
 var maxDepthForRule = make(map[int]map[string]int, 10)
 
 //MatchText Matches text
-func (r Rule) MatchText(currentDepth int, input string, lazyRules map[int]bool) (bool, string) {
-	matches := false
-	//defer func() {
-	//	currentMaxDepth, ok := maxDepthForRule[r.ruleID]
-	//	if !ok {
-	//		maxDepthForRule[r.ruleID] = make(map[string]int, 1)
-	//		currentMaxDepth = maxDepthForRule[r.ruleID]
-	//	}
-	//	if d, _ := currentMaxDepth["depth"]; d < currentDepth {
-	//		currentMaxDepth["depth"] = d
-	//	}
-	//	currentMaxDepth["matches"] = 1
-	//	if !matches {
-	//		currentMaxDepth["matches"] = 0
-	//	}
-	//	//fmt.Printf("result %v at depth %d for rule %d\n", matches, currentDepth, r.ruleID)
-	//}()
+func (r Rule) MatchText(input string) (matches bool, leftOverString string) {
 
-	numberOfChecks++
-	var localBranchInput string
-	var matchesL, matchesR bool
 	if len(r.LRule) > 0 {
-		matchesL, localBranchInput = r.MatchBranch(currentDepth, r.LRule, input, nil)
+		matches, leftOverString = r.MatchBranch(r.LRule, input)
+		if matches {
+			return
+		}
 	}
-	rLeftInput := localBranchInput
 	if len(r.RRule) > 0 {
-		matchesR, localBranchInput = r.MatchBranch(currentDepth, r.RRule, input, nil)
+		matches, leftOverString = r.MatchBranch(r.RRule, input)
+		if matches {
+			return
+		}
 	}
-	if matchesL {
-		localBranchInput = rLeftInput
+
+	if len(r.RRule) == 0 && len(r.LRule) == 0 && input != "" {
+		matches = input[:1] == r.value
+		leftOverString = input[1:]
 	}
-	matches = matchesL || matchesR
-	if len(input) == 0 {
-		//fmt.Printf("not matching because no more input\n")
-		return false, ""
-	}
-	if len(r.RRule) == 0 && len(r.LRule) == 0 {
-		matches = input[:1] == r.chars
-		localBranchInput = input[1:]
-	}
-	return matches, localBranchInput
+	return
 
 }
-func (r Rule) StringWithLimit(depth int, maxDepth int) string {
+func (r Rule) regexWithLimit(depth int, maxDepth int) string {
 
 	if len(r.LRule) == 0 {
-		if len(r.chars) == 0 {
+		if len(r.value) == 0 {
 			panic(fmt.Sprintf("rule %d is not well generated", r.ruleID))
 		}
-		return r.chars
+		return r.value
 	}
 	if depth >= maxDepth {
 		return ""
@@ -103,12 +78,12 @@ func (r Rule) StringWithLimit(depth int, maxDepth int) string {
 	chars.WriteString("(?:")
 	depth++
 	for _, aRule := range r.LRule {
-		chars.WriteString(aRule.StringWithLimit(depth, maxDepth))
+		chars.WriteString(aRule.regexWithLimit(depth, maxDepth))
 	}
 	if len(r.RRule) > 0 {
 		chars.WriteString("|")
 		for _, aRule := range r.RRule {
-			chars.WriteString(aRule.StringWithLimit(depth, maxDepth))
+			chars.WriteString(aRule.regexWithLimit(depth, maxDepth))
 		}
 	}
 	chars.WriteString(")")
@@ -127,114 +102,69 @@ func main() {
 	input, _ := ioutil.ReadFile(*inputFile)
 	trimmedInput := strings.Split(strings.TrimSpace(string(input)), "\n")
 	system := parse(trimmedInput)
-	//fmt.Printf("Read rules %#v", system.Rules)
-	validRuleTest := 0
-	validRuleRegex := 0
-	rule := parseRule(system.Rules, "0")
-	testMatcherP1 := regexp.MustCompile(fmt.Sprintf("^%s$", rule.StringWithLimit(0, 13)))
-	fmt.Printf("Using rule: '%s'\n", rule)
+	validMessages := 0
+	system = parse(trimmedInput)
+	P1 := parseRule(system.Rules, *ruleToCheck)
 	for _, m := range system.Messages {
-		//fmt.Printf("Testing %s\n", m)
-		match, matchingChars := rule.MatchText(0, m, nil)
-		if match {
-			if len(matchingChars) == 0 {
-				validRuleTest++
-			}
-		}
-		if testMatcherP1.MatchString(m) {
-			validRuleRegex++
+		match, unmatchedChars := P1.MatchText(m)
+		if match && len(unmatchedChars) == 0 {
+			validMessages++
 		}
 	}
-	fmt.Printf("Part1(RuleTest): %d\n", validRuleTest)
-	fmt.Printf("Part2(RuleRegex): %d\n", validRuleRegex)
-	validRuleTest = 0
+	fmt.Printf("Part1: %d\n", validMessages)
+	validMessages = 0
 	system = parse(trimmedInput)
 	system.Rules["8"] = &Rule{
-		ruleID:             8,
+		ruleID:             "8",
 		OriginalExpression: "42 | 42 8",
 	}
 	system.Rules["11"] = &Rule{
-		ruleID:             11,
+		ruleID:             "11",
 		OriginalExpression: "42 31 | 42 11 31",
 	}
-	ruleP2 := parseRule(system.Rules, "0")
-	fmt.Printf("Using rule %s\n", ruleP2.StringWithLimit(0, 9))
-	testMatcher := regexp.MustCompile(fmt.Sprintf("^%s$", ruleP2.StringWithLimit(0, 13)))
-	part2Valid := 0
+	ruleP2 := parseRule(system.Rules, *ruleToCheck)
+	//13 is the minimun value from experimentation
+	testMatcher := regexp.MustCompile(fmt.Sprintf("^%s$", ruleP2.regexWithLimit(0, 13)))
 	part2Regex := 0
 	for _, m := range system.Messages {
-		match, matchingChars := ruleP2.MatchText(0, m, nil)
-		//fmt.Printf("Leftover chars %s\n", matchingChars)
-		if match {
-			if len(matchingChars) == 0 {
-				//fmt.Printf("%s\n", m)
-				part2Valid++
-			}
+		match, unmatchedChars := ruleP2.MatchText(m)
+		if match && len(unmatchedChars) == 0 {
+			validMessages++
 		}
-		m2 := testMatcher.MatchString(m)
-		if m2 {
+		if testMatcher.MatchString(m) {
 			part2Regex++
 		}
-		if match != m2 {
-			//fmt.Printf("%s\n", m)
-		}
 	}
-	fmt.Printf("Part2(RuleTest): %d\n", part2Valid)
+	fmt.Printf("Part2(recursive): %d\n", validMessages)
 	fmt.Printf("Part2(part2Regex): %d\n", part2Regex)
-	fmt.Printf("Checks: %d\n", numberOfChecks)
 }
 
 func parseRuleList(parsedRules map[string]*Rule, rulesID string) []*Rule {
 	rIds := strings.Split(rulesID, " ")
 	var result []*Rule
 	for _, r := range rIds {
-		var parsedRule *Rule
-		//var ok bool
-		parsedRule, _ = parsedRules[r]
-		if parsedRule != nil && parsedRule.parsed == false {
-			parsedRule = parseRule(parsedRules, r)
+		if r == "" {
+			continue
 		}
-		if parsedRule != nil {
-			result = append(result, parsedRule)
-		}
+		result = append(result, parseRule(parsedRules, r))
 	}
 	return result
 }
 func parseRule(parsedRules map[string]*Rule, ruleID string) *Rule {
-	//fmt.Printf("Going in for rule %s\n", ruleID)
 
 	r, ok := parsedRules[ruleID]
 	if ok && r.parsed {
-		fmt.Printf("Rule %s, already parsed\n", ruleID)
 		return r
 	}
-	rid, _ := strconv.Atoi(ruleID)
 	rule := &Rule{
-		ruleID:             rid,
+		ruleID:             ruleID,
 		parsed:             true,
 		OriginalExpression: r.OriginalExpression,
 	}
 	parsedRules[ruleID] = rule
-	if strings.Index(r.OriginalExpression, "|") == -1 && strings.Index(r.OriginalExpression, " ") == -1 {
-		_, err := strconv.Atoi(r.OriginalExpression)
-		if err == nil {
-			//If it's a single char we can ignore the rule and just map it to the reference
-			var parsedRule *Rule
-			var ok bool
-			parsedRule, ok = parsedRules[r.OriginalExpression]
-			if !ok || parsedRule.parsed == false {
-				parsedRule = parseRule(parsedRules, r.OriginalExpression)
-			}
-			return &Rule{
-				ruleID:             rid,
-				LRule:              []*Rule{parsedRule},
-				OriginalExpression: r.OriginalExpression,
-				parsed:             true,
-			}
-		}
-		rule.ruleID = rid
-		rule.chars = strings.Replace(r.OriginalExpression, "\"", "", -1)
-		//rule.Expression = r.String()
+	if strings.Index(r.OriginalExpression, "\"") != -1 {
+		rule.ruleID = ruleID
+		rule.value = strings.Replace(r.OriginalExpression, "\"", "", -1)
 		return rule
 	}
 	sides := strings.Split(r.OriginalExpression, "|")
@@ -242,7 +172,6 @@ func parseRule(parsedRules map[string]*Rule, ruleID string) *Rule {
 	if len(sides) > 1 {
 		rule.RRule = parseRuleList(parsedRules, sides[1])
 	}
-	//rule.Expression = rule.String()
 
 	return rule
 }
@@ -257,14 +186,12 @@ func parse(input []string) MythicalInformationSystem {
 	readRules := make(map[string]*Rule)
 	for _, r := range ruleInput {
 		splitRules := strings.Split(r, ":")
-		ruleID, _ := strconv.Atoi(splitRules[0])
 		readRules[splitRules[0]] = &Rule{
-			ruleID:             ruleID,
+			ruleID:             splitRules[0],
 			OriginalExpression: strings.TrimSpace(splitRules[1]),
 		}
 	}
 
-	//fmt.Printf("R2: %s", readRules["2"].String())
 	return MythicalInformationSystem{
 		Rules:    readRules,
 		Messages: messageInput,
---
+++ 2020/19/first.rulegroup.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var inputFile = flag.String("f", "input", "Relative file path to use as input.")
+var ruleToCheck = flag.Int("r", 0, "Use this rule to check")
+var partA = flag.Bool("pA", false, "Part A")
+
+//Rule A rule.
+type Rule struct {
+	ruleID             int
+	rules              [][]*Rule
+	OriginalExpression string
+	value              string
+	parsed             bool
+}
+
+//MatchText Matches text and returns the number of matching characters
+func (r Rule) MatchText(input string) (result []int) {
+
+	if len(r.rules) == 0 {
+		if len(input) < len(r.value) {
+			result = nil
+			return
+		}
+		if input[:len(r.value)] == r.value {
+			result = []int{len(r.value)}
+			return
+		}
+	}
+	var matches []int
+	for _, groupRule := range r.rules {
+		potentialMatches := []int{0}
+		for _, localRule := range groupRule {
+			var newPotentialMatches []int
+			for _, m := range potentialMatches {
+				matches := localRule.MatchText(input[m:])
+				for _, v := range matches {
+					newPotentialMatches = append(newPotentialMatches, v+m)
+				}
+			}
+			potentialMatches = newPotentialMatches
+		}
+		matches = append(matches, potentialMatches...)
+	}
+	result = matches
+	return
+}
+
+// StringWithLimit returns a regex string with a maximum depth to avoid infinite recursion
+func (r Rule) StringWithLimit(depth int, maxDepth int) string {
+
+	if len(r.rules) == 0 {
+		if len(r.value) == 0 {
+			panic(fmt.Sprintf("rule %d is not well generated", r.ruleID))
+		}
+		return r.value
+	}
+	if depth >= maxDepth {
+		return ""
+	}
+	var ruleStringBuilder strings.Builder
+	ruleStringBuilder.WriteString("(?:")
+	depth++
+	for k, ruleGroup := range r.rules {
+		if k > 0 {
+			ruleStringBuilder.WriteString("|")
+		}
+		for _, aRule := range ruleGroup {
+			ruleStringBuilder.WriteString(aRule.StringWithLimit(depth, maxDepth))
+		}
+	}
+	ruleStringBuilder.WriteString(")")
+	return ruleStringBuilder.String()
+}
+
+//MythicalInformationSystem The information received from the elves
+type MythicalInformationSystem struct {
+	Rules    map[int]*Rule
+	Messages []string
+}
+
+func main() {
+	flag.Parse()
+	input, _ := ioutil.ReadFile(*inputFile)
+	trimmedInput := strings.Split(strings.TrimSpace(string(input)), "\n")
+	system := parse(trimmedInput)
+	validRuleTest := 0
+	validRuleRegex := 0
+	rule := parseRule(system.Rules, *ruleToCheck)
+	testMatcherP1 := regexp.MustCompile(fmt.Sprintf("^%s$", rule.StringWithLimit(0, 13)))
+	for _, m := range system.Messages {
+		match := rule.MatchText(m)
+		for _, r := range match {
+			if r == len(m) {
+				validRuleTest++
+				break
+			}
+		}
+		if testMatcherP1.MatchString(m) {
+			validRuleRegex++
+		}
+	}
+	fmt.Printf("Part1(RuleTest): %d\n", validRuleTest)
+	fmt.Printf("Part2(RuleRegex): %d\n", validRuleRegex)
+	validRuleTest = 0
+	validRuleRegex = 0
+	system = parse(trimmedInput)
+	system.Rules[8] = &Rule{
+		OriginalExpression: "42 | 42 8",
+	}
+	system.Rules[11] = &Rule{
+		OriginalExpression: "42 31 | 42 11 31",
+	}
+	ruleP2 := parseRule(system.Rules, *ruleToCheck)
+	testMatcher := regexp.MustCompile(fmt.Sprintf("^%s$", ruleP2.StringWithLimit(0, 13)))
+	for _, m := range system.Messages {
+		match := ruleP2.MatchText(m)
+		for _, r := range match {
+			if r == len(m) {
+				validRuleTest++
+				break
+			}
+		}
+		m2 := testMatcher.MatchString(m)
+		if m2 {
+			validRuleRegex++
+			//return
+		}
+	}
+	fmt.Printf("Part2(validRuleTest): %d\n", validRuleTest)
+	fmt.Printf("Part2(validRuleRegex): %d\n", validRuleRegex)
+
+}
+
+func parseRule(parsedRules map[int]*Rule, ruleID int) *Rule {
+
+	r, ok := parsedRules[ruleID]
+	if ok && r.parsed {
+		return r
+	}
+
+	rule := &Rule{
+		ruleID:             ruleID,
+		parsed:             true,
+		OriginalExpression: r.OriginalExpression,
+	}
+	parsedRules[ruleID] = rule
+	if strings.Index(r.OriginalExpression, "\"") != -1 {
+		rule.value = strings.Replace(r.OriginalExpression, "\"", "", -1)
+		return rule
+	}
+
+	for _, s := range strings.Split(strings.TrimSpace(r.OriginalExpression), "|") {
+		rIds := strings.Split(strings.TrimSpace(s), " ")
+		var groupRules []*Rule
+		for _, r := range rIds {
+			d, _ := strconv.Atoi(r)
+			groupRules = append(groupRules, parseRule(parsedRules, d))
+		}
+		rule.rules = append(rule.rules, groupRules)
+	}
+	return rule
+	if strings.Index(r.OriginalExpression, "|") == -1 && strings.Index(r.OriginalExpression, " ") == -1 {
+		candidateID, err := strconv.Atoi(r.OriginalExpression)
+		if err == nil {
+			//If it's a single value we can ignore the rule and just map it to the reference
+			var parsedRule *Rule
+			var ok bool
+			parsedRule, ok = parsedRules[candidateID]
+			if !ok || parsedRule.parsed == false {
+				parsedRule = parseRule(parsedRules, candidateID)
+			}
+			g := make([][]*Rule, 1, 1)
+			g[0] = []*Rule{parsedRule}
+			return &Rule{
+				ruleID:             ruleID,
+				OriginalExpression: r.OriginalExpression,
+				parsed:             true,
+				rules:              g,
+			}
+		}
+
+	}
+
+	return rule
+}
+func parse(input []string) MythicalInformationSystem {
+	var i int
+	var ruleInput []string
+	var messageInput []string
+	for i = 0; input[i] != ""; i++ {
+		ruleInput = append(ruleInput, input[i])
+	}
+	messageInput = input[i+1:]
+	readRules := make(map[int]*Rule)
+	for _, r := range ruleInput {
+		splitRules := strings.Split(r, ":")
+		ruleID, _ := strconv.Atoi(splitRules[0])
+		readRules[ruleID] = &Rule{
+			ruleID:             ruleID,
+			OriginalExpression: strings.TrimSpace(splitRules[1]),
+		}
+	}
+
+	//fmt.Printf("R2: %s", readRules["2"].String())
+	return MythicalInformationSystem{
+		Rules:    readRules,
+		Messages: messageInput,
+	}
+}
