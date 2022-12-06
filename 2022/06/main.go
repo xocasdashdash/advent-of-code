80bab253f6ef896e9f4d82e53f541705beddb245
Joaquín Fernández Campo <jfcampo@gmail.com>
2022-12-06 09:03:27 +0100

    2022 - Dia 6

---
+++ 2022/06/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+var inputFile = flag.String("f", "input", "Relative file path to use as input.")
+
+func findUniqueSequenceOfLength(input string, length int) int {
+
+	chars := strings.Split(input, "")
+	for i := 0; i < len(chars)-length; i++ {
+		substring := strings.Join(chars[i:i+length], "")
+		uniqueMap := make(map[string]bool)
+		// TODO: This is too brute force, need a datastruct that
+		// keeps uniqueness and also can tell you how many unique
+		// characters it's keeping.
+		for _, c := range strings.Split(substring, "") {
+			uniqueMap[c] = true
+		}
+		if len(uniqueMap) == length {
+			return i + length
+		}
+	}
+	return -1
+}
+func main() {
+	flag.Parse()
+	input, _ := ioutil.ReadFile(*inputFile)
+	trimmedInput := strings.Split(strings.TrimSpace(string(input)), "\n")
+	for _, l := range trimmedInput {
+		fmt.Println("Part1", findUniqueSequenceOfLength(l, 4))
+	}
+	for _, l := range trimmedInput {
+		fmt.Println("Part2", findUniqueSequenceOfLength(l, 14))
+	}
+}
